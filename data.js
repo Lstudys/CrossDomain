f0ef8904d83ae1bf312fa4b4a55df71af6b4cf8b
Lstudys <Lstudys@163.com>
2021-11-07 21:23:59 +0800

    添加动态刷新列表demo

---
+++ data.js
@@ -0,0 +1,22 @@
+const data = [
+    {
+        number:1,
+        name:'lyh',
+        age:20,
+        sex:'男'
+    },
+    {
+        number:2,
+        name:'student2',
+        age:20,
+        sex:'男'
+    },
+    {
+        number:3,
+        name:'student3',
+        age:20,
+        sex:'男'
+    },
+]
+
+export {data};
--- server2.js
+++ server2.js
@@ -1,16 +1,35 @@
 import express from 'express';
+import path from 'path';
+import fs from 'fs';
 
-import asyncHandler from 'express-async-handler';
-import axios from 'axios';
+import {data} from './data.js';
 
 const app = express();
 
 
-app.get('/getData',asyncHandler(async (req,res)=>{
-    const {data} = await axios.get("http://localhost:3001/sendData");
-    console.log(data);
+app.get('/index.html',(req,res)=>{
+    // let filepath=path.join(__dirname,'example','example.html');
+    let data=fs.readFileSync('./example/index.html','utf-8');
     res.send(data);
-}))
+});
+
+
+app.get('/initData',(req,res)=>{
+    res.json(data);
+})
+
+
+app.get('/addData/:number',(req,res)=>{
+    const number = Number(req.params.number) + 1;
+    const newData = {
+        number:number,
+        name:'student' + number,
+        age:20,
+        sex:'男'
+    }
+    res.json(newData);
+})
+
 
 app.listen(8080,()=>{
     console.log('server2 running');
